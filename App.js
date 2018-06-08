ef670647a76e5f0e908f58e4faa672042507a491
Kelvan Ince <incekelvan@gmail.com>
2018-06-07 20:54:23 -0500

    Add react native navigation

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 
 import AuthScreen from './src/Screens/Auth/Auth';
 import SharePlaceScreen from './src/Screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/Screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/Screens/PlaceDetail/PlaceDetail';
 import SideDrawer from "./src/Screens/SideDrawer/SideDrawer";
+// import configureStore from './src/Store/ConfigureStore';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <AuthScreen />
-      </View>
-    );
+Navigation.registerComponent(
+  'places.AuthScreen', () => AuthScreen
+);
+
+export default () => Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'places.AuthScreen',
+    title: 'Login'
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
