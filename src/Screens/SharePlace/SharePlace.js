23abdc5b93c0ba2cad3c5f8df97c20e06eb88c78
Kelvan Ince <incekelvan@gmail.com>
2018-06-08 00:22:04 -0500

    Add side drawer and share place screens

--- src/Screens/SharePlace/SharePlace.js
+++ src/Screens/SharePlace/SharePlace.js
@@ -1,18 +1,215 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ScrollView,
+  Image,
+  ActivityIndicator
+} from "react-native";
+import { connect } from "react-redux";
+
+import { addPlace, startAddPlace } from "../../Store/Actions/index";
+import PlaceInput from "../../Components/PlaceInput/PlaceInput";
+import MainText from "../../Components/UI/MainText/MainText";
+import HeadingText from "../../Components/UI/HeadingText/HeadingText";
+import PickImage from "../../Components/PickImage/PickImage";
+import PickLocation from "../../Components/PickLocation/PickLocation";
+import validate from "../../Utility/Validation";
 
 class SharePlaceScreen extends Component {
+  static navigatorStyle = {
+    navBarButtonColor: "orange"
+  };
+
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  componentWillMount() {
+    this.reset();
+  }
+
+  reset = () => {
+    this.setState({
+      controls: {
+        placeName: {
+          value: "",
+          valid: false,
+          touched: false,
+          validationRules: {
+            notEmpty: true
+          }
+        },
+        location: {
+          value: null,
+          valid: false
+        },
+        image: {
+          value: null,
+          valid: false
+        }
+      }
+    });
+  };
+
+  componentDidUpdate() {
+    if (this.props.placeAdded) {
+      this.props.navigator.switchToTab({ tabIndex: 0 });
+      // this.props.onStartAddPlace();
+    }
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === "ScreenChangedEvent") {
+      if (event.id === "willAppear") {
+        this.props.onStartAddPlace();
+      }
+    }
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        });
+      }
+    }
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          placeName: {
+            ...prevState.controls.placeName,
+            value: val,
+            valid: validate(val, prevState.controls.placeName.validationRules),
+            touched: true
+          }
+        }
+      };
+    });
+  };
+
+  locationPickedHandler = location => {
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          location: {
+            value: location,
+            valid: true
+          }
+        }
+      };
+    });
+  };
+
+  imagePickedHandler = image => {
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          image: {
+            value: image,
+            valid: true
+          }
+        }
+      };
+    });
+  };
+
+  placeAddedHandler = () => {
+    this.props.onAddPlace(
+      this.state.controls.placeName.value,
+      this.state.controls.location.value,
+      this.state.controls.image.value
+    );
+    this.reset();
+    this.imagePicker.reset();
+    this.locationPicker.reset();
+    // this.props.navigator.switchToTab({tabIndex: 0});
+  };
+
   render() {
+    let submitButton = (
+      <Button
+        title="Share the Place!"
+        onPress={this.placeAddedHandler}
+        disabled={
+          !this.state.controls.placeName.valid ||
+          !this.state.controls.location.valid ||
+          !this.state.controls.image.valid
+        }
+      />
+    );
+
+    if (this.props.isLoading) {
+      submitButton = <ActivityIndicator />;
+    }
+
     return (
-      <View>
-        <Text>SharePlaceScreen</Text>
-        <Text>SharePlaceScreen</Text>
-        <Text>SharePlaceScreen</Text>
-        <Text>SharePlaceScreen</Text>
-        <Text>SharePlaceScreen</Text>
-      </View>
-    )
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a Place with us!</HeadingText>
+          </MainText>
+          <PickImage
+            onImagePicked={this.imagePickedHandler}
+            ref={ref => (this.imagePicker = ref)}
+          />
+          <PickLocation
+            onLocationPick={this.locationPickedHandler}
+            ref={ref => (this.locationPicker = ref)}
+          />
+          <PlaceInput
+            placeData={this.state.controls.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <View style={styles.button}>{submitButton}</View>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
-export default SharePlaceScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  placeholder: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    width: "80%",
+    height: 150
+  },
+  button: {
+    margin: 8
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%"
+  }
+});
+
+const mapStateToProps = state => {
+  return {
+    isLoading: state.ui.isLoading,
+    placeAdded: state.places.placeAdded
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (placeName, location, image) =>
+      dispatch(addPlace(placeName, location, image)),
+    onStartAddPlace: () => dispatch(startAddPlace())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SharePlaceScreen);
--- src/Screens/SideDrawer/SideDrawer.js
+++ src/Screens/SideDrawer/SideDrawer.js
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { connect } from 'react-redux';
+
+import { authLogout } from '../../Store/Actions/index';
 
 class SideDrawer extends Component {
   render() {
     return (
-      <View>
-        <Text>SideDrawer</Text>
-        <Text>SideDrawer</Text>
-        <Text>SideDrawer</Text>
-        <Text>SideDrawer</Text>
-        <Text>SideDrawer</Text>
+      <View
+        style={[
+          styles.container,
+          { width: Dimensions.get("window").width * 0.8 }
+        ]}
+      >
+        <TouchableOpacity onPress={this.props.onLogout}>
+          <View style={styles.drawerItem}>
+            <Icon
+              name={Platform.OS === 'android' ? 'md-log-out' : "ios-log-out"}
+              size={30}
+              color='#aaa'
+              style={styles.drawerIcon}
+            />
+            <Text>Sign Out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
-export default SideDrawer;
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    backgroundColor: "white",
+    flex: 1
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee'
+  },
+  drawerIcon: {
+    marginRight: 10
+  }
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => dispatch(authLogout())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SideDrawer);
